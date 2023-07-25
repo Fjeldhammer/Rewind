6c0c0c9dd1a63ccd5bdc3ed0ee82e52ba44e7dc2
Fjeldhammer <fjeld.uilleam@gmail.com>
2023-07-25 10:34:57 -0600

    added code and img

--- js/site.js
+++ js/site.js
@@ -1,14 +1,36 @@
-function displayMessage() {
-    let inputBox = document.getElementById('message');
+function getValues() {
 
-    let message = inputBox.value;
+    let userString = document.getElementById('userString').value;
 
-    Swal.fire(
-        {
-            backdrop: false,
-            title: 'App Name',
-            text: message
-        }
-    );
+    let revString = reverseString(userString);
+
+    displayString(revString);
 }
 
+function reverseString(input) { // input = 'apple'
+
+    let strArray = input.split(''); // strArray = ['a', 'p', 'p', 'l', 'e']
+    let reversedArray = [];
+
+    for (let index = strArray.length - 1; index >= 0; index = index - 1) {
+
+        let letter = strArray[index];
+
+        reversedArray.push(letter);
+        
+    }
+
+    let revString = reversedArray.join('');
+
+    return revString;
+
+}
+
+function displayString(output) {
+
+    document.getElementById('results').textContent = output;
+
+    let alertBox = document.getElementById('alert');
+
+    alertBox.classList.remove('invisible');
+}
